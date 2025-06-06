8e1c018174a76df37dd6ba9d4c801e8506b102fc
EmmanuelBalde3434 <138893225+EmmanuelBalde3434@users.noreply.github.com>
2025-06-05 22:07:18 -0600

    Update apps.php

--- apps.php
+++ apps.php
@@ -1,1 +1,5 @@
-
+<div class="task" id="task-1">
+  <input type="checkbox" class="complete-checkbox">
+  <span class="task-text">Tarea de ejemplo</span>
+  <button class="complete-btn">Marcar como completada</button>
+</div>
